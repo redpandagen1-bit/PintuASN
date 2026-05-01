8f04953f2c04d4228e96e09ab28d766e2ba08479
redpandagen1-bit <redpandagen1@gmail.com>
2026-05-01 18:57:21 +0700

    webhook fix

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -28,8 +28,7 @@ export async function POST(req: Request) {
     return new Response('Missing svix headers', { status: 400 });
   }
 
-  const payload = await req.json();
-  const body = JSON.stringify(payload);
+  const body = await req.text();
 
   const wh = new Webhook(WEBHOOK_SECRET);
   let evt: WebhookEvent;
